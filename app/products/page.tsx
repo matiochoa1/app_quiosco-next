f495cffab5f66847f062d045d32e26f3ce2cd9ec
Matias <mattiochoax@gmail.com>
2024-12-27 13:00:11 +0100

    Creating the first pages and layouts (). Adding prisma models

---
+++ app/products/page.tsx
@@ -0,0 +1,9 @@
+export default function ProductsPage() {
+	console.log("Desde el servidor");
+
+	return (
+		<>
+			<div>ProductsPage</div>
+		</>
+	);
+}
---
+++ components/order/OrderSideBar.tsx
@@ -0,0 +1,7 @@
+export default function OrderSideBar() {
+	return (
+		<>
+			<aside className="md:w-72 md:h-screen bg-white ">OrderSideBar</aside>
+		</>
+	);
+}
